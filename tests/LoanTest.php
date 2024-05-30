a133702e8d7cf2f0f407f601fb70317144081b56
rs.haru <hyde.rou@ya.ru>
2024-05-30 16:36:21 +0300

    add test for getLoanById

--- tests/LoanTest.php
+++ tests/LoanTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Models\Loan;
+// use Illuminate\Foundation\Testing\TestCase;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
@@ -17,5 +18,25 @@ class LoanTest extends TestCase {
         $this->assertEquals(200, $response->status());
 
         $response->assertExactJson([]);
+    }
+
+    public function testPostLoans() {
+
+        $response = $this->call('POST', 'api/v1/loans', ['amount' => 999]);
+        $this->assertEquals(201, $response->status());
+
+    }
+
+    public function testGetLoanById() {
+
+        $loan = Loan::create(['amount' => 777]);
+
+        $response = $this->json('GET', "api/v1/loans/$loan->id")
+        ->seeJsonEquals([
+            'id' => $loan->id,
+            'amount' => $loan->amount,
+            'created_at' => $loan->created_at,
+            'updated_at' => $loan->updated_at,
+         ]);
     }
 }
